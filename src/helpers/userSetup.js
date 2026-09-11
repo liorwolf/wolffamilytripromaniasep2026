6f195f06391f289827f8be1bc13b0e20b3100d18
Lior Wolf <liorwolf@gmail.com>
2026-09-11 16:41:30 +0300

    Passthrough-copy robots.txt to the site root

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,9 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+
+  // Copy robots.txt (Disallow all) to the site root so crawlers skip the site
+  eleventyConfig.addPassthroughCopy({ "src/site/robots.txt": "robots.txt" });
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
